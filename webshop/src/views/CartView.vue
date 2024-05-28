8d457a8f3523ffd108e89fb371d95004ae7cfed5
Pryann <pryann@gmail.com>
2024-05-28 09:23:31 +0200

    webshop init

---
+++ webshop/src/views/CartView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="cart">
+    <h1>This the cart page</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- webshop/src/views/HomeView.vue
+++ webshop/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Main page</h1>
   </main>
 </template>
